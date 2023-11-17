17110457c82431719c1b263aa2ae7a118c65cddc
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-11-17 18:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var dateDiffInDays = function (date1, date2) {
   //   write your code here
 	let a = Date.parse(date1)
   let b = Date.parse(date2);
-  let result = (a-b)/(60*60*24*1000)
+  let result = (b-a)/(60*60*24*1000)
   return result;
 };
 
